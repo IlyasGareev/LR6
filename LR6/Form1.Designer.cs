014383650dad1752c5b842f250aa84e129ed9199
IlyasGareev <gareev_ilyas@list.ru>
2022-01-23 17:12:56 +0500

    Lr6

---
+++ LR6/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace LR6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pbBlack = new System.Windows.Forms.PictureBox();
+            this.pbBlue = new System.Windows.Forms.PictureBox();
+            this.pbRed = new System.Windows.Forms.PictureBox();
+            this.pbSquare = new System.Windows.Forms.PictureBox();
+            this.pbCircle = new System.Windows.Forms.PictureBox();
+            this.pbTriangle = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBlack)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBlue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSquare)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCircle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTriangle)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.pbTriangle);
+            this.panel1.Controls.Add(this.pbCircle);
+            this.panel1.Controls.Add(this.pbSquare);
+            this.panel1.Controls.Add(this.pbRed);
+            this.panel1.Controls.Add(this.pbBlue);
+            this.panel1.Controls.Add(this.pbBlack);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1583, 106);
+            this.panel1.TabIndex = 0;
+            // 
+            // pbBlack
+            // 
+            this.pbBlack.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pbBlack.Location = new System.Drawing.Point(0, 0);
+            this.pbBlack.Name = "pbBlack";
+            this.pbBlack.Size = new System.Drawing.Size(83, 62);
+            this.pbBlack.TabIndex = 0;
+            this.pbBlack.TabStop = false;
+            this.pbBlack.Click += new System.EventHandler(this.pbBlack_Click);
+            // 
+            // pbBlue
+            // 
+            this.pbBlue.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.pbBlue.Location = new System.Drawing.Point(89, -1);
+            this.pbBlue.Name = "pbBlue";
+            this.pbBlue.Size = new System.Drawing.Size(81, 63);
+            this.pbBlue.TabIndex = 1;
+            this.pbBlue.TabStop = false;
+            this.pbBlue.Click += new System.EventHandler(this.pbBlue_Click);
+            // 
+            // pbRed
+            // 
+            this.pbRed.BackColor = System.Drawing.Color.Red;
+            this.pbRed.Location = new System.Drawing.Point(176, 0);
+            this.pbRed.Name = "pbRed";
+            this.pbRed.Size = new System.Drawing.Size(79, 62);
+            this.pbRed.TabIndex = 2;
+            this.pbRed.TabStop = false;
+            this.pbRed.Click += new System.EventHandler(this.pbRed_Click);
+            // 
+            // pbSquare
+            // 
+            this.pbSquare.Image = ((System.Drawing.Image)(resources.GetObject("pbSquare.Image")));
+            this.pbSquare.InitialImage = ((System.Drawing.Image)(resources.GetObject("pbSquare.InitialImage")));
+            this.pbSquare.Location = new System.Drawing.Point(305, 3);
+            this.pbSquare.Name = "pbSquare";
+            this.pbSquare.Size = new System.Drawing.Size(110, 79);
+            this.pbSquare.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbSquare.TabIndex = 3;
+            this.pbSquare.TabStop = false;
+            this.pbSquare.Click += new System.EventHandler(this.pbSquare_Click);
+            // 
+            // pbCircle
+            // 
+            this.pbCircle.Image = ((System.Drawing.Image)(resources.GetObject("pbCircle.Image")));
+            this.pbCircle.Location = new System.Drawing.Point(455, 3);
+            this.pbCircle.Name = "pbCircle";
+            this.pbCircle.Size = new System.Drawing.Size(110, 79);
+            this.pbCircle.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbCircle.TabIndex = 4;
+            this.pbCircle.TabStop = false;
+            this.pbCircle.Click += new System.EventHandler(this.pbCircle_Click);
+            // 
+            // pbTriangle
+            // 
+            this.pbTriangle.Image = ((System.Drawing.Image)(resources.GetObject("pbTriangle.Image")));
+            this.pbTriangle.Location = new System.Drawing.Point(604, 3);
+            this.pbTriangle.Name = "pbTriangle";
+            this.pbTriangle.Size = new System.Drawing.Size(110, 79);
+            this.pbTriangle.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbTriangle.TabIndex = 5;
+            this.pbTriangle.TabStop = false;
+            this.pbTriangle.Click += new System.EventHandler(this.pbTriangle_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Window;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(1, 112);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1582, 521);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            this.panel2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1595, 645);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbBlack)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBlue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSquare)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCircle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTriangle)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pbTriangle;
+        private System.Windows.Forms.PictureBox pbCircle;
+        private System.Windows.Forms.PictureBox pbSquare;
+        private System.Windows.Forms.PictureBox pbRed;
+        private System.Windows.Forms.PictureBox pbBlue;
+        private System.Windows.Forms.PictureBox pbBlack;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ LR6/Form1.cs
@@ -0,0 +1,586 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LR6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        class Figure   //базовый класс
+        {
+
+            virtual public bool isClicked(MouseEventArgs e)
+            {
+                return true;
+            }
+            virtual public void Draw(Panel panel1, Graphics g)
+            {
+            }
+            virtual public void DoCheckTrue()
+            {
+            }
+            virtual public void DoCheckFalse()
+            {
+            }
+            virtual public bool isChecked()
+            {
+                return true;
+            }
+            virtual public void Move(KeyEventArgs e)
+            {
+            }
+            virtual public void ChangeSize(KeyEventArgs e)
+            {
+            }
+            virtual public void ChangeColor(int color)
+            {
+            }
+        }
+
+        class CCircle : Figure   //класс круга
+        {
+            private int x;
+            private int y;
+            private int rad;
+            private bool Checked;
+            private Pen pen;
+            private SolidBrush brush;
+            public CCircle(int x, int y, int rad)
+            {
+                this.x = x;
+                this.y = y;
+                this.rad = rad;
+                Checked = false;
+                brush = new SolidBrush(Color.White);
+            }
+            override public bool isClicked(MouseEventArgs e)
+            {
+                if (((e.X - x) * (e.X - x) + (e.Y - y) * (e.Y - y)) <= rad * rad)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            public override void Draw(Panel panel1, Graphics g)
+            {
+                Rectangle rect = new Rectangle(x - rad, y - rad, rad * 2, rad * 2);
+                if (Checked == true)
+                {
+                    pen = new Pen(Color.Tomato);
+                }
+                else
+                {
+                    pen = new Pen(Color.Black);
+                }
+                g.FillEllipse(brush, rect);
+                g.DrawEllipse(pen, rect);
+
+            }
+            override public void DoCheckTrue()
+            {
+                Checked = true;
+            }
+            override public void DoCheckFalse()
+            {
+                Checked = false;
+            }
+            override public bool isChecked()
+            {
+                return Checked;
+            }
+            public override void Move(KeyEventArgs e)
+            {
+                if (e.KeyValue == 38)
+                {
+                    y = y - 10;
+                }
+                if (e.KeyValue == 40)
+                {
+                    y = y + 10;
+                }
+                if (e.KeyValue == 39)
+                {
+                    x = x + 10;
+                }
+                if (e.KeyValue == 37)
+                {
+                    x = x - 10;
+                }
+            }
+            public override void ChangeSize(KeyEventArgs e)
+            {
+                if (e.KeyValue == 187)
+                {
+                    rad = rad + 5;
+                }
+                if (e.KeyValue == 189)
+                {
+                    rad = rad - 5;
+                }
+            }
+            public override void ChangeColor(int color)
+            {
+                switch (color)
+                {
+                    case 1:
+                        brush.Color = Color.Black;
+                        break;
+                    case 2:
+                        brush.Color = Color.Blue;
+                        break;
+                    case 3:
+                        brush.Color = Color.Red;
+                        break;
+                }
+
+            }
+        }
+
+        class Square : Figure    //класс квадрата 
+        {
+            private int x;
+            private int y;
+            private int rad;
+            private bool Checked;
+            private SolidBrush brush;
+            public Square(int x, int y, int rad)
+            {
+                this.x = x;
+                this.y = y;
+                this.rad = rad;
+                Checked = false;
+                brush = new SolidBrush(Color.White);
+            }
+            override public bool isClicked(MouseEventArgs e)
+            {
+                if (((e.X - x) * (e.X - x) + (e.Y - y) * (e.Y - y)) <= rad * rad * 2)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            public override void Draw(Panel panel1, Graphics g)
+            {
+                Rectangle rect = new Rectangle(x - rad, y - rad, rad * 2, rad * 2);
+                if (Checked == true)
+                {
+                    g.FillRectangle(brush, rect);
+                    g.DrawRectangle(Pens.Tomato, rect);
+                }
+                else
+                {
+                    g.FillRectangle(brush, rect);
+                    g.DrawRectangle(Pens.Black, rect);
+                }
+            }
+            override public void DoCheckTrue()
+            {
+                Checked = true;
+            }
+            override public void DoCheckFalse()
+            {
+                Checked = false;
+            }
+            override public bool isChecked()
+            {
+                return Checked;
+            }
+            public override void Move(KeyEventArgs e)
+            {
+                if (e.KeyValue == 38)
+                {
+                    y = y - 10;
+                }
+                if (e.KeyValue == 40)
+                {
+                    y = y + 10;
+                }
+                if (e.KeyValue == 39)
+                {
+                    x = x + 10;
+                }
+                if (e.KeyValue == 37)
+                {
+                    x = x - 10;
+                }
+            }
+            public override void ChangeSize(KeyEventArgs e)
+            {
+                if (e.KeyValue == 187)
+                {
+                    rad = rad + 5;
+                }
+                if (e.KeyValue == 189)
+                {
+                    rad = rad - 5;
+                }
+            }
+            public override void ChangeColor(int color)
+            {
+                switch (color)
+                {
+                    case 1:
+                        brush.Color = Color.Black;
+                        break;
+                    case 2:
+                        brush.Color = Color.Blue;
+                        break;
+                    case 3:
+                        brush.Color = Color.Red;
+                        break;
+                }
+            }
+        }
+
+        class Triangle : Figure   //класс треугольника
+        {
+            private Point[] p;
+            private bool Checked;
+            private int side;
+            private int x, y;
+            private SolidBrush brush;
+            public Triangle(int x, int y, int side)
+            {
+                p = new Point[3];
+                p[0].X = x;
+                p[0].Y = y - 60;
+                p[1].X = x;
+                p[1].Y = y + 60;
+                p[2].X = x + 90;
+                p[2].Y = y + 60;
+                Checked = false;
+                this.side = side;
+                this.x = x;
+                this.y = y;
+                brush = new SolidBrush(Color.White);
+            }
+            override public bool isClicked(MouseEventArgs e)
+            {
+                if (e.X >= x - side / 2 && e.Y >= y - side / 2 && e.X <= x + side / 2 && e.Y <= y + side / 2)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            public override void Draw(Panel panel1, Graphics g)
+            {
+                if (Checked == true)
+                {
+                    g.FillPolygon(brush, p);
+                    g.DrawPolygon(Pens.Tomato, p);
+                }
+                else
+                {
+                    g.FillPolygon(brush, p);
+                    g.DrawPolygon(Pens.Black, p);
+                }
+            }
+            override public void DoCheckTrue()
+            {
+                Checked = true;
+            }
+            override public void DoCheckFalse()
+            {
+                Checked = false;
+            }
+            override public bool isChecked()
+            {
+                return Checked;
+            }
+            public override void Move(KeyEventArgs e)
+            {
+                if (e.KeyValue == 38)
+                {
+                    p[0].Y = p[0].Y - 5;
+                    p[1].Y = p[1].Y - 5;
+                    p[2].Y = p[2].Y - 5;
+                }
+                if (e.KeyValue == 40)
+                {
+                    p[0].Y = p[0].Y + 5;
+                    p[1].Y = p[1].Y + 5;
+                    p[2].Y = p[2].Y + 5;
+                }
+                if (e.KeyValue == 37)
+                {
+                    p[0].X = p[0].X - 5;
+                    p[1].X = p[1].X - 5;
+                    p[2].X = p[2].X - 5;
+                }
+                if (e.KeyValue == 39)
+                {
+                    p[0].X = p[0].X + 5;
+                    p[1].X = p[1].X + 5;
+                    p[2].X = p[2].X + 5;
+                }
+            }
+            public override void ChangeSize(KeyEventArgs e)
+            {
+                if (e.KeyValue == 187)
+                {
+                    p[0].X = p[0].X;
+                    p[0].Y = p[0].Y - 3;
+                    p[1].X = p[1].X;
+                    p[1].Y = p[1].Y + 3;
+                    p[2].X = p[2].X + 6;
+                    p[2].Y = p[2].Y + 3;
+                }
+                if (e.KeyValue == 189)
+                {
+                    p[0].X = p[0].X;
+                    p[0].Y = p[0].Y + 3;
+                    p[1].X = p[1].X;
+                    p[1].Y = p[1].Y - 3;
+                    p[2].X = p[2].X - 6;
+                    p[2].Y = p[2].Y - 3;
+                }
+            }
+            public override void ChangeColor(int color)
+            {
+                switch (color)
+                {
+                    case 1:
+                        brush.Color = Color.Black;
+                        break;
+                    case 2:
+                        brush.Color = Color.Blue;
+                        break;
+                    case 3:
+                        brush.Color = Color.Red;
+                        break;
+                }
+            }
+        }
+
+        class MyStorage
+        {
+            private int size;
+            Figure[] storage;
+            public MyStorage()
+            {
+                size = 0;
+            }
+            public MyStorage(int size)
+            {
+                this.size = size;
+                storage = new Figure[size];
+            }
+            public void SetObjects(int index, Figure obj)
+            {
+                storage[index] = obj;
+            }
+            public void AddObject(Figure obj)
+            {
+                Figure[] new_storage = new Figure[size + 1];
+                for (int i = 0; i < size; i++)
+                {
+                    new_storage[i] = storage[i];
+                }
+                storage = new_storage;
+                storage[size] = obj;
+                size = size + 1;
+            }
+            public void DeleteObject(int index)
+            {
+                Figure[] new_storage = new Figure[size - 1];
+                for (int i = 0; i < index; i++)
+                {
+                    new_storage[i] = storage[i];
+                }
+                for (int i = index; i < size - 1; i++)
+                {
+                    new_storage[i] = storage[i + 1];
+                }
+                size = size - 1;
+                storage = new_storage;
+            }
+            public bool isCheckedStorage(MouseEventArgs e)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    if (storage[i].isClicked(e) == true)
+                    {
+                        storage[i].DoCheckTrue();
+                        return true;
+                    }
+                }
+                return false;
+            }
+            public void DeleteCheckObject(MyStorage storage)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    if (this.storage[i].isChecked() == true)
+                    {
+                        storage.DeleteObject(i);
+                        i = i - 1;
+                    }
+                }
+            }
+            public void NotChecked()
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    storage[i].DoCheckFalse();
+                }
+            }
+            public void Draw(Panel panel1, Graphics g)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    storage[i].Draw(panel1, g);
+                }
+            }
+            public void Move(KeyEventArgs e)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    if (storage[i].isChecked() == true)
+                    {
+                        storage[i].Move(e);
+                    }
+                }
+            }
+            public void ChangeSize(KeyEventArgs e)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    if (storage[i].isChecked() == true)
+                    {
+                        storage[i].ChangeSize(e);
+                    }
+                }
+            }
+            public void ChangeColor(int color)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    if (storage[i].isChecked() == true)
+                    {
+                        storage[i].ChangeColor(color);
+                    }
+                }
+            }
+        }
+
+        MyStorage storage = new MyStorage();
+        int figure;
+        int color;
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = panel2.CreateGraphics();
+            storage.Draw(panel2, g);
+        }
+
+        private void panel2_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (storage.isCheckedStorage(e) == false)
+            {
+                storage.NotChecked();
+                switch (figure)
+                {
+                    case 1:
+                        CCircle ellipse = new CCircle(e.X, e.Y, 45);
+                        storage.AddObject(ellipse);
+                        break;
+                    case 2:
+                        Square square = new Square(e.X, e.Y, 45);
+                        storage.AddObject(square);
+                        break;
+                    case 3:
+                        Triangle triangle = new Triangle(e.X, e.Y, 90);
+                        storage.AddObject(triangle);
+                        break;
+                }
+            }
+            else
+            {
+                if (Control.ModifierKeys == Keys.Control)
+                {
+                    storage.isCheckedStorage(e);
+                }
+                else
+                {
+                    storage.NotChecked();
+                    storage.isCheckedStorage(e);
+                    if (color == 1 || color == 2 || color == 3)
+                    {
+                        storage.ChangeColor(color);
+                    }
+                }
+            }
+            Refresh();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            if (e.KeyValue == 46)
+            {
+                storage.DeleteCheckObject(storage);
+                g.Clear(Color.White);
+            }
+            if (e.KeyValue == 38 || e.KeyValue == 40 || e.KeyValue == 39 || e.KeyValue == 37)
+            {
+                g.Clear(Color.White);
+                storage.Move(e);
+            }
+            if (e.KeyValue == 187 || e.KeyValue == 189)
+            {
+                g.Clear(Color.White);
+                storage.ChangeSize(e);
+            }
+            Refresh();
+        }
+
+        private void pbSquare_Click(object sender, EventArgs e)
+        {
+            figure = 2;
+        }
+
+        private void pbCircle_Click(object sender, EventArgs e)
+        {
+            figure = 1;
+        }
+
+        private void pbTriangle_Click(object sender, EventArgs e)
+        {
+            figure = 3;
+        }
+
+        private void pbBlack_Click(object sender, EventArgs e)
+        {
+            color = 1;
+        }
+
+        private void pbBlue_Click(object sender, EventArgs e)
+        {
+            color = 2;
+        }
+
+        private void pbRed_Click(object sender, EventArgs e)
+        {
+            color = 3;
+        }
+    }
+}
